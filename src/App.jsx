f0fef96420d52b55fd54360382d2036460d4e795
Thisara shehan kavinda <thisara2000shehankavinda@gmail.com>
2024-11-24 15:17:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Main from './Components/Main/Main'
+import SideBar from './Components/SideBar/SideBar'
+
+const App = () => {
+  return (
+    <>
+      <SideBar/> 
+      <Main/>
+    </>
+  )
+}
+
+export default App
+
